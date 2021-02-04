05c2af1509b82b689fd4435ce166b285f3ce285c
InfinityDevFlo <byfloredstone@gmail.com>
2021-02-04 16:42:00 +0100

    Updated Project Structure

---
+++ vextension-minecraft-server/src/main/kotlin/eu/vironlab/vextension/scoreboard/builder/SidebarBuilder.kt
@@ -0,0 +1,77 @@
+/**
+ *   Copyright © 2020 | vironlab.eu | All Rights Reserved.<p>
+ * <p>
+ *      ___    _______                        ______         ______  <p>
+ *      __ |  / /___(_)______________ _______ ___  / ______ ____  /_ <p>
+ *      __ | / / __  / __  ___/_  __ \__  __ \__  /  _  __ `/__  __ \<p>
+ *      __ |/ /  _  /  _  /    / /_/ /_  / / /_  /___/ /_/ / _  /_/ /<p>
+ *      _____/   /_/   /_/     \____/ /_/ /_/ /_____/\__,_/  /_.___/ <p>
+ *<p>
+ *    ____  _______     _______ _     ___  ____  __  __ _____ _   _ _____ <p>
+ *   |  _ \| ____\ \   / / ____| |   / _ \|  _ \|  \/  | ____| \ | |_   _|<p>
+ *   | | | |  _|  \ \ / /|  _| | |  | | | | |_) | |\/| |  _| |  \| | | |  <p>
+ *   | |_| | |___  \ V / | |___| |__| |_| |  __/| |  | | |___| |\  | | |  <p>
+ *   |____/|_____|  \_/  |_____|_____\___/|_|   |_|  |_|_____|_| \_| |_|  <p>
+ *<p>
+ *<p>
+ *   This program is free software: you can redistribute it and/or modify<p>
+ *   it under the terms of the GNU General Public License as published by<p>
+ *   the Free Software Foundation, either version 3 of the License, or<p>
+ *   (at your option) any later version.<p>
+ *<p>
+ *   This program is distributed in the hope that it will be useful,<p>
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of<p>
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<p>
+ *   GNU General Public License for more details.<p>
+ *<p>
+ *   You should have received a copy of the GNU General Public License<p>
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.<p>
+ *<p>
+ *   Contact:<p>
+ *<p>
+ *     Discordserver:   https://discord.gg/wvcX92VyEH<p>
+ *     Website:         https://vironlab.eu/ <p>
+ *     Mail:            contact@vironlab.eu<p>
+ *<p>
+ */
+
+package eu.vironlab.vextension.scoreboard.builder
+
+import eu.vironlab.vextension.builder.Builder
+import eu.vironlab.vextension.collection.DataPair
+import eu.vironlab.vextension.scoreboard.ScoreboardUtil
+import eu.vironlab.vextension.scoreboard.Sidebar
+import eu.vironlab.vextension.scoreboard.SidebarLine
+import eu.vironlab.vextension.scoreboard.bukkit.BukkitSidebar
+
+
+class SidebarBuilder : Builder<Sidebar> {
+
+    val lines: MutableMap<String, SidebarLine> = mutableMapOf()
+    var title: String = ""
+
+    fun addLine(init: LineBuilder.() -> Unit) {
+        val builder: LineBuilder = LineBuilder()
+        builder.init()
+        val line = builder.build()
+        this.lines.put(line.name, line)
+    }
+
+    override fun build(): Sidebar {
+        val finalLines: MutableMap<String, DataPair<String, SidebarLine>> = mutableMapOf()
+        val usedColors: MutableList<String> = mutableListOf()
+        lines.forEach {
+            val color = ScoreboardUtil.getAvailableColor(usedColors)
+            usedColors.add(color)
+            finalLines.put(it.key, DataPair(color, it.value))
+        }
+        return BukkitSidebar(finalLines, usedColors, title)
+    }
+
+}
+
+fun sidebar(init: SidebarBuilder.() -> Unit): Sidebar {
+    val builder: SidebarBuilder = SidebarBuilder()
+    builder.init()
+    return builder.build()
+}
---
+++ vextension-minecraft-server/src/main/kotlin/eu/vironlab/vextension/scoreboard/bukkit/BukkitSidebar.kt
@@ -0,0 +1,202 @@
+/**
+ *   Copyright © 2020 | vironlab.eu | All Rights Reserved.<p>
+ * <p>
+ *      ___    _______                        ______         ______  <p>
+ *      __ |  / /___(_)______________ _______ ___  / ______ ____  /_ <p>
+ *      __ | / / __  / __  ___/_  __ \__  __ \__  /  _  __ `/__  __ \<p>
+ *      __ |/ /  _  /  _  /    / /_/ /_  / / /_  /___/ /_/ / _  /_/ /<p>
+ *      _____/   /_/   /_/     \____/ /_/ /_/ /_____/\__,_/  /_.___/ <p>
+ *<p>
+ *    ____  _______     _______ _     ___  ____  __  __ _____ _   _ _____ <p>
+ *   |  _ \| ____\ \   / / ____| |   / _ \|  _ \|  \/  | ____| \ | |_   _|<p>
+ *   | | | |  _|  \ \ / /|  _| | |  | | | | |_) | |\/| |  _| |  \| | | |  <p>
+ *   | |_| | |___  \ V / | |___| |__| |_| |  __/| |  | | |___| |\  | | |  <p>
+ *   |____/|_____|  \_/  |_____|_____\___/|_|   |_|  |_|_____|_| \_| |_|  <p>
+ *<p>
+ *<p>
+ *   This program is free software: you can redistribute it and/or modify<p>
+ *   it under the terms of the GNU General Public License as published by<p>
+ *   the Free Software Foundation, either version 3 of the License, or<p>
+ *   (at your option) any later version.<p>
+ *<p>
+ *   This program is distributed in the hope that it will be useful,<p>
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of<p>
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<p>
+ *   GNU General Public License for more details.<p>
+ *<p>
+ *   You should have received a copy of the GNU General Public License<p>
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.<p>
+ *<p>
+ *   Contact:<p>
+ *<p>
+ *     Discordserver:   https://discord.gg/wvcX92VyEH<p>
+ *     Website:         https://vironlab.eu/ <p>
+ *     Mail:            contact@vironlab.eu<p>
+ *<p>
+ */
+
+package eu.vironlab.vextension.scoreboard.bukkit
+
+import eu.vironlab.vextension.bukkit.VextensionBukkit
+import eu.vironlab.vextension.collection.DataPair
+import eu.vironlab.vextension.concurrent.AsyncUtil
+import eu.vironlab.vextension.scoreboard.ScoreboardUtil
+import eu.vironlab.vextension.scoreboard.Sidebar
+import eu.vironlab.vextension.scoreboard.SidebarLine
+import java.util.*
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.scoreboard.DisplaySlot
+import org.bukkit.scoreboard.Objective
+import org.bukkit.scoreboard.Team
+
+
+class BukkitSidebar(
+    override val lines: MutableMap<String, DataPair<String, SidebarLine>>,
+    val usedColors: MutableCollection<String>,
+    override var title: String
+) : Sidebar,
+    Listener {
+
+    val players: MutableList<Player> = mutableListOf()
+    var listening: Boolean = false
+
+    init {
+        Bukkit.getPluginManager().registerEvents(this, VextensionBukkit.instance)
+    }
+
+    override fun addLine(line: SidebarLine) {
+        if (!this.lines.containsKey(line.name)) {
+            val color = ScoreboardUtil.getAvailableColor(usedColors)
+            this.lines.put(line.name, DataPair(color, line))
+            this.usedColors.add(color)
+            AsyncUtil.schedule {
+                this.players.forEach {
+                    val scoreboard = it.scoreboard
+                    val team: Team = scoreboard.registerNewTeam(line.name)
+                    if (line.proceed != null) {
+                        line.proceed!!.invoke(line, it.uniqueId)
+                    }
+                    val splittetLine = ScoreboardUtil.splitContent(line.content)
+                    team.prefix = splittetLine.first
+                    team.suffix = splittetLine.second
+                    team.addEntry(color)
+                    scoreboard.getObjective(DisplaySlot.SIDEBAR)!!.getScore(color).score = line.score
+                }
+            }
+        }
+    }
+
+    override fun updateLine(name: String, line: SidebarLine) {
+        AsyncUtil.schedule {
+            players.forEach {
+                val scoreboard = it.scoreboard
+                val team: Team? = scoreboard.getTeam(name)
+                if (team != null) {
+                    if (line.proceed != null) {
+                        line.proceed!!.invoke(line, it.uniqueId)
+                    }
+                    val splittetLine = ScoreboardUtil.splitContent(line.content)
+                    team.prefix = splittetLine.first
+                    team.suffix = splittetLine.second
+                    scoreboard.getObjective(DisplaySlot.SIDEBAR)!!.getScore(lines.get(name)!!.first).score = line.score
+                }
+            }
+        }
+    }
+
+    override fun set(player: UUID) {
+        AsyncUtil.schedule {
+            val p: Player? = Bukkit.getPlayer(player)
+            if (p != null) {
+                val scoreboard = p.scoreboard
+                if (scoreboard.getObjective(DisplaySlot.SIDEBAR) != null) {
+                    scoreboard.getObjective(DisplaySlot.SIDEBAR)!!.unregister()
+                }
+                if (scoreboard.getObjective("sidebar") != null) {
+                    scoreboard.getObjective("sidebar")!!.unregister()
+                }
+                val objective: Objective = scoreboard.registerNewObjective("sidebar", "dummy", this.title)
+                objective.displaySlot = DisplaySlot.SIDEBAR
+                this.lines.forEach { name, pair ->
+                    if (scoreboard.getTeam(name) != null) {
+                        scoreboard.getTeam(name)!!.unregister()
+                    }
+                    val team: Team = scoreboard.getTeam(name) ?: scoreboard.registerNewTeam(name)
+                    Bukkit.getConsoleSender().sendMessage("1")
+                    if (pair.second.proceed != null) {
+                        Bukkit.getConsoleSender().sendMessage("2")
+                        pair.second.proceed!!.invoke(pair.second, player)
+                    }
+                    Bukkit.getConsoleSender().sendMessage("3")
+                    val splittetLine = ScoreboardUtil.splitContent(pair.second.content)
+                    team.prefix = splittetLine.first
+                    team.suffix = splittetLine.second
+                    team.addEntry(pair.first)
+                    objective.getScore(pair.first).score = pair.second.score
+                }
+                this.players.add(p)
+            }
+        }
+    }
+
+    override fun setAll() {
+        Bukkit.getOnlinePlayers().forEach {
+            set(it.uniqueId)
+        }
+    }
+
+    override fun setAllAndListen() {
+        Bukkit.getOnlinePlayers().forEach {
+            set(it.uniqueId)
+        }
+        this.listening = true
+    }
+
+    override fun cancelListening() {
+        if (this.listening) {
+            this.listening = false
+        }
+    }
+
+    override fun removeAll() {
+        players.forEach {
+            remove(it.uniqueId)
+        }
+    }
+
+    override fun remove(player: UUID) {
+        val p: Player? = Bukkit.getPlayer(player)
+        if (p != null && this.players.contains(player)) {
+            p.scoreboard.getObjective(DisplaySlot.SIDEBAR)!!.unregister()
+            p.scoreboard.clearSlot(DisplaySlot.SIDEBAR)
+            this.players.remove(player)
+        }
+    }
+
+    override fun updateTitle(title: String) {
+        this.title = title
+        players.forEach {
+            it.scoreboard.getObjective(DisplaySlot.SIDEBAR)!!.displayName = title
+        }
+    }
+
+    @EventHandler
+    fun handleJoin(event: PlayerJoinEvent) {
+        if (listening) {
+            set(event.player.uniqueId)
+        }
+    }
+
+    @EventHandler
+    fun handleQuit(event: PlayerQuitEvent) {
+        if (this.players.contains(event.player.uniqueId)) {
+            remove(event.player.uniqueId)
+        }
+    }
+
+}
