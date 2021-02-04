ddea39cb0931fa6c7c7614b941014a2f95894e3a
InfinityDevFlo <byfloredstone@gmail.com>
2021-02-04 19:05:52 +0100

    Added Sponge Server Initializer

---
+++ vextension-minecraft-server/src/main/kotlin/eu/vironlab/vextension/sponge/VextensionSponge.kt
@@ -0,0 +1,88 @@
+/**
+ *   Copyright © 2020 | vironlab.eu | All Rights Reserved.<p>
+ * <p>
+ *      ___    _______                        ______         ______  <p>
+ *      __ |  / /___(_)______________ _______ ___  / ______ ____  /_ <p>
+ *      __ | / / __  / __  ___/_  __ \__  __ \__  /  _  __ `/__  __ \<p>
+ *      __ |/ /  _  /  _  /    / /_/ /_  / / /_  /___/ /_/ / _  /_/ /<p>
+ *      _____/   /_/   /_/     \____/ /_/ /_/ /_____/\__,_/  /_.___/ <p>
+ *<p>
+ *    ____  _______     _______ _     ___  ____  __  __ _____ _   _ _____ <p>
+ *   |  _ \| ____\ \   / / ____| |   / _ \|  _ \|  \/  | ____| \ | |_   _|<p>
+ *   | | | |  _|  \ \ / /|  _| | |  | | | | |_) | |\/| |  _| |  \| | | |  <p>
+ *   | |_| | |___  \ V / | |___| |__| |_| |  __/| |  | | |___| |\  | | |  <p>
+ *   |____/|_____|  \_/  |_____|_____\___/|_|   |_|  |_|_____|_| \_| |_|  <p>
+ *<p>
+ *<p>
+ *   This program is free software: you can redistribute it and/or modify<p>
+ *   it under the terms of the GNU General Public License as published by<p>
+ *   the Free Software Foundation, either version 3 of the License, or<p>
+ *   (at your option) any later version.<p>
+ *<p>
+ *   This program is distributed in the hope that it will be useful,<p>
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of<p>
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<p>
+ *   GNU General Public License for more details.<p>
+ *<p>
+ *   You should have received a copy of the GNU General Public License<p>
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.<p>
+ *<p>
+ *   Contact:<p>
+ *<p>
+ *     Discordserver:   https://discord.gg/wvcX92VyEH<p>
+ *     Website:         https://vironlab.eu/ <p>
+ *     Mail:            contact@vironlab.eu<p>
+ *<p>
+ */
+
+package eu.vironlab.vextension.sponge
+
+import com.google.inject.Inject
+import eu.vironlab.vextension.Vextension
+import eu.vironlab.vextension.database.ClientNotInitializedException
+import eu.vironlab.vextension.database.DatabaseClient
+import eu.vironlab.vextension.database.DatabaseClientType
+import eu.vironlab.vextension.database.DatabaseConnectionData
+import eu.vironlab.vextension.database.mongodb.MongoDatabaseClient
+import eu.vironlab.vextension.database.sql.SqlDatabaseClient
+import eu.vironlab.vextension.rest.wrapper.vironlab.VironLabAPI
+import org.slf4j.Logger
+import org.spongepowered.api.event.Listener
+import org.spongepowered.api.event.game.state.GameStartedServerEvent
+import org.spongepowered.api.plugin.Plugin
+
+@Plugin(
+    id = "vextension_sponge",
+    name = "Vextension-Sponge",
+    version = "1.0.0-SNAPSHOT",
+    description = "Vextension for Sponge Plugins",
+    authors = arrayOf("VironLab")
+)
+class VextensionSponge : Vextension  {
+
+    private var databaseClient: DatabaseClient? = null
+    @Inject
+    private lateinit var logger: Logger
+
+    @Listener
+    fun init(event: GameStartedServerEvent) {
+        logger.info("Loaded Vextension by VironLab: https://github.com/VironLab/Vextension")
+    }
+
+    override fun getDatabaseClient(): DatabaseClient {
+        return this.databaseClient ?: throw ClientNotInitializedException("You have to init the client first")
+    }
+
+
+    override fun initDatabase(type: DatabaseClientType, connectionData: DatabaseConnectionData) {
+        when(type) {
+            DatabaseClientType.SQL -> {
+                this.databaseClient = SqlDatabaseClient(connectionData.toSql())
+            }
+            DatabaseClientType.MONGO -> {
+                this.databaseClient = MongoDatabaseClient(connectionData.database, connectionData.toMongo())
+            }
+        }
+    }
+
+}
