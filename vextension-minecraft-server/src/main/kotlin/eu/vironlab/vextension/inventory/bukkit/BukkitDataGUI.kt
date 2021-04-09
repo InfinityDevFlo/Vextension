450c438e17ad6229ee757649f681935b5d4b2dc3
SteinGaming <69219673+SteinGaming@users.noreply.github.com>
2021-04-09 19:08:37 +0200

    first working commit

--- vextension-minecraft-server/src/main/kotlin/eu/vironlab/vextension/inventory/bukkit/BukkitDataGUI.kt
+++ vextension-minecraft-server/src/main/kotlin/eu/vironlab/vextension/inventory/bukkit/BukkitDataGUI.kt
@@ -36,19 +36,93 @@
  */
 package eu.vironlab.vextension.inventory.bukkit
 
-import eu.vironlab.vextension.inventory.entry.Entry
+import eu.vironlab.vextension.concurrent.scheduleAsync
 import eu.vironlab.vextension.inventory.gui.DataGUI
 import eu.vironlab.vextension.item.ItemStack
+import eu.vironlab.vextension.item.Material
+import eu.vironlab.vextension.item.builder.item
+import org.bukkit.Bukkit
 import java.util.*
+import java.util.function.BiConsumer
 
-class BukkitDataGUI(override val lines: Int, override val name: String): DataGUI {
-    override val comparator: Comparator<Entry>? = null
-    override var contents: MutableList<ItemStack> = mutableListOf()
-
-    override var border: Boolean = false
-    override var borderItem: ItemStack? = null
-
+class BukkitDataGUI(override val lines: Int, override val name: String) : DataGUI {
+    override var comparator: Comparator<ItemStack>? = null
+    override var border: ItemStack? = null
+    override var defaultList: MutableCollection<ItemStack> = mutableListOf()
     override fun open(player: UUID) {
-        TODO("Not yet implemented")
+        open(player, defaultList)
+    }
+
+    init {
+        if (lines == 1) {
+            throw IllegalArgumentException("Lines have to be higher than 1")
+        }
+    }
+
+    override fun open(player: UUID, list: MutableCollection<ItemStack>) {
+        scheduleAsync {
+            val contents =
+                list.sortedWith(comparator ?: throw NullPointerException("Comparator cannot be null")).toMutableList()
+            var last = 0
+            val pages: MutableList<BukkitGUI> = mutableListOf()
+            for ((first, second) in (0..contents.size step if (border != null) (((lines - 1) * 9) - (lines * 2)) + lines * 2 - 4 - 9 else (lines - 1) * 9).withIndex()) {
+                pages.add(BukkitPage().also {
+                    it.border = this.border
+                }.create(list.toMutableList().subList(last, second), first, this))
+                last = second + 1
+            }
+            for ((index, page) in pages.withIndex()) {
+                val indexUp: BiConsumer<ItemStack, UUID> = BiConsumer { _, uuid ->
+                    pages[index + 1].open(uuid)
+                }
+                val indexDown: BiConsumer<ItemStack, UUID> = BiConsumer { _, uuid ->
+                    pages[index - 1].open(uuid)
+                }
+                when (index) {
+                    0 -> {
+                        page.setItem(lines * 9 - 1, item(Material.ARROW) {
+                            setName("Goto Page ${index + 1} ->")
+                            setBlockAll(true)
+                            setClickHandler(indexUp)
+                        })
+                    }
+                    pages.lastIndex -> {
+                        page.setItem((lines - 1) * 9, item(Material.ARROW) {
+                            setName("<- Goto Page ${index - 1}")
+                            setBlockAll(true)
+                            setClickHandler(indexDown)
+                        })
+                    }
+                    else -> {
+                        page.setItem(lines * 9 - 1, item(Material.ARROW) {
+                            setName("Goto Page ${index + 1} ->")
+                            setBlockAll(true)
+                            setClickHandler(indexUp)
+                        })
+                        page.setItem((lines - 1) * 9, item(Material.ARROW) {
+                            setName("<- Goto Page ${index - 1}")
+                            setBlockAll(true)
+                            setClickHandler(indexDown)
+                        })
+                    }
+                }
+            }
+            pages[0].open(player)
+        }
+    }
+
+    fun setComparator(comparator: Comparator<ItemStack>): BukkitDataGUI {
+        this.comparator = comparator
+        return this
+    }
+
+    fun setBorder(border: ItemStack): BukkitDataGUI {
+        this.border = border
+        return this
+    }
+
+    fun setDefaultList(list: MutableCollection<ItemStack>): BukkitDataGUI {
+        this.defaultList = list
+        return this
     }
 }
