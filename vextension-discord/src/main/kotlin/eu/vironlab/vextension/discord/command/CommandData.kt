444ceb3ea78865afddf4d088f90cd3d2e850e566
Ali Daryab <danialdaryab@gmail.com>
2021-06-12 22:21:06 +0200

    Deleted Discord Module because moving to https://github.com/VironLab/DiscordCommands

--- vextension-discord/src/main/kotlin/eu/vironlab/vextension/discord/command/CommandData.kt
+++
@@ -1,5 +0,0 @@
-package eu.vironlab.vextension.discord.command
-
-import eu.vironlab.vextension.discord.command.executor.CommandExecutor
-
-data class CommandData(val name: String, val description: String, val aliases: Array<String>, val target: CommandChannelTarget, val executor: CommandExecutor)
--- vextension-discord/src/main/kotlin/eu/vironlab/vextension/discord/command/annotation/Command.kt
+++
@@ -1,7 +0,0 @@
-package eu.vironlab.vextension.discord.command.annotation
-
-import eu.vironlab.vextension.discord.command.CommandChannelTarget
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.CLASS)
-annotation class Command(val name: String, val target: CommandChannelTarget, val description: String, val aliases: Array<String>)
