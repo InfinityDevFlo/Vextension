319afd8a1dea868e8ef5206c9de8daea2a57aba9
SteinGaming <69219673+SteinGaming@users.noreply.github.com>
2021-12-15 16:30:17 +0100

    Removed broken repositories

--- buildSrc/src/main/kotlin/Repositories.kt
+++ buildSrc/src/main/kotlin/Repositories.kt
@@ -37,8 +37,8 @@
 
 
 object Repositories {
-    const val KOTLINX = "https://dl.bintray.com/kotlin/kotlinx/"
-    const val KTOR = "https://dl.bintray.com/kotlin/ktor/"
+    //const val KOTLINX = "https://dl.bintray.com/kotlin/kotlinx/"
+    //const val KTOR = "https://dl.bintray.com/kotlin/ktor/"
     const val MAVEN_CENTRAL = "https://repo1.maven.org/maven2/"
     const val VIRONLAB_SNAPSHOT = "https://repo.vironlab.eu/repository/maven-snapshot/"
     const val MINECRAFT_SPONGE = "https://repo.spongepowered.org/maven/"
